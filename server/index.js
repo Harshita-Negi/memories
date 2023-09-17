743c5c45f7c44770c5672cd5e29941e8c3058c18
Harshita <harshitanegi229@gmail.com>
2023-09-17 14:06:00 +0530

    ADDED DOTENV

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoute from "./routes/posts.js";
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
@@ -14,14 +16,11 @@ app.use(cors());
 app.use('/posts', postRoute);
 
 // CONNECTING TO SERVER TO MONGODB
-const CONNECTION_URL = 'mongodb+srv://harshitanegi:harshitanegi2003@cluster0.uwxvdyf.mongodb.net/test';
 const PORT = 5000 || process.env.PORT;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) 
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) 
   .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
   .catch((e)=> console.log(e.message));
-
-
 
 
 
